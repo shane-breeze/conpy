5f2c0469ffa4bd447fcc3c9c1b3788866d15fb73
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-08-09 18:28:09 +0100

    Update reqs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", 'r') as fh:
 
 setuptools.setup(
     name="conpy",
-    version="0.1.0",
+    version="0.1.1",
     author="Shane Breeze",
     author_email="sdb15@ic.ac.uk",
     scripts=["conpy/conpy_worker.py", "conpy/conpy_worker.sh"],
